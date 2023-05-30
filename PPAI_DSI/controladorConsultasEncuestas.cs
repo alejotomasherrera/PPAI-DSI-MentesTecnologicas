4912b14f0065a37f792b1ff5f7cb619eb8ccc8ea
Alejo <alejotomasherrera@gmail.com>
2023-05-30 12:12:56 -0300

    Ultimo update

--- PPAI_DSI/controladorConsultasEncuestas.cs
+++ PPAI_DSI/controladorConsultasEncuestas.cs
@@ -24,12 +24,8 @@ namespace PPAI_DSI
         private List<Llamada>? llamadas = new List<Llamada>();
         private List<RespuestaPosible>? respuestasPosibles = new List<RespuestaPosible>();
         private List<Encuesta>? encuestas = new List<Encuesta>() ;
-        private List<Pregunta>? preguntas = new List<Pregunta>();
         private List<RespuestaDeCliente>? respuestasDeClientes = new List<RespuestaDeCliente>();
         private VentanaConsultarLlamadas? pantalla;
-        private List<Cliente>? clientes = new List<Cliente>();
-        private List<Estado>? estados =  new List<Estado>();
-        private List<CambioEstado>? cambiosEstado = new List<CambioEstado>();
         private List<Llamada>? llamadasEncontradas = new List<Llamada>();
         private Llamada? llamadaElegida;
         private Encuesta? encuestaEncontrada;
@@ -38,44 +34,24 @@ namespace PPAI_DSI
         private Mockup? datos;
 
         
-        //metodo constructor con inicializacion de todos los atributos pasados por parametro y sino pasan parametros que los inicialize vacios segun su tipo de dato
+        //metodo constructor con inicializacion de todos los atributos pasad1os por parametro y sino pasan parametros que los inicialize vacios segun su tipo de dato
         public controladorConsultasEncuestas(VentanaConsultarLlamadas consultarLlamadas)
         {
             datos = new Mockup();
             llamadas = datos._llamadas;
             encuestas = datos._encuestas;
-            //Preguntas no deberia estar aqui en teoria
-            preguntas = datos._preguntas;
+  
             this.pantalla = consultarLlamadas;
 
         }
 
         //metodos get y set
-        public List<Estado> _estados
-        {
-            get => estados;
-            set => estados = value;
-        }
-        public List<CambioEstado> _cambiosEstado
-        {
-            get => cambiosEstado;
-            set => cambiosEstado = value;
-        }
-        public List<Cliente> _clienteList
-        {
-            get => clientes;
-            set => clientes = value;
-        }
         public List<RespuestaDeCliente> _respuestasDeClientes
         {
             get => respuestasDeClientes;
             set => respuestasDeClientes = value;
         }
-        public List<Pregunta> _preguntas
-        {
-            get => preguntas;
-            set => preguntas = value;
-        }
+        
         public List<Encuesta> _encuestas
         {
             get => encuestas;
@@ -106,19 +82,16 @@ namespace PPAI_DSI
         // metodo llamado "consultarEncuesta()" que le envia un mensaje llamado "solicitarPeriodoLlamada" a la pantalla "ConsultarLlamadas"
         public void consultarEncuesta()
         {
-            // como le envio el mensaje a la ventana "ConsultarLlamadas"? 
+            
             pantalla.solicitarPeriodoLlamada();
 
 
         }
 
-        //metodo "fechaInicioYFinSeleccionadas" que recibe 2 parametros de tipo DateTime y los guarda en 2 variables locales e instancia el metodo buscarLLamadasDelPeriodoRespondidas() pasandole las 2 variables locales como parametros
         public void fechaInicioYFinSeleccionadas(DateTime fechaDesde, DateTime fechaHasta)
         {
-            // guarda los parametros en variables locales
             fechaDesdeLocal = fechaDesde;
             fechaHastaLocal = fechaHasta;
-            // instancia el metodo buscarLLamadasDelPeriodoRespondidas() pasandole las 2 variables locales como parametros
             buscarLLamadasDelPeriodoRespondidas(fechaDesdeLocal, fechaHastaLocal);
         }
 
@@ -126,8 +99,6 @@ namespace PPAI_DSI
         public void buscarLLamadasDelPeriodoRespondidas(DateTime fechaDesde, DateTime fechaHasta)
         {
             llamadasEncontradas = new List<Llamada>();
-            // crea un bucle para recorrer todas las llamadas y verificar si estan respondidas y son de un periodo dado
-            // si es asi las agrega a la lista de llamadas encontradas, el metodo para validar si tiene una respuesta es "tieneRespuestaDeCliente"
             foreach (Llamada llamada in llamadas)
             {
                 if (llamada.tieneRespuestaDeCliente() && llamada.esDePeriodo(fechaDesde,fechaHasta))
@@ -172,30 +143,23 @@ namespace PPAI_DSI
             List<Pregunta> preguntasDeLaLlamada = new List<Pregunta>();
 
             preguntasDeLaLlamada = llamadaElegida.obtenerPreguntas(datos._preguntas);
-            //MessageBox.Show("preguntas de la llamada: " + preguntasDeLaLlamada.Count.ToString());
-            //Encuesta encuestaEncontrada = new Encuesta();
             foreach (var encuesta in encuestas)
             {
                 if (encuesta.sonTusPreguntas(preguntasDeLaLlamada))
                 {
-                    //Cuando encuentro la encuesta que tiene las preguntas de la llamada, la guardo en una variable local
                     encuestaEncontrada = encuesta;
                     break;
                 }
             }
 
 
-            //Crear un metodo que contenga la logica de armar la encuesta
             listaPreguntas = new List<string>();
             listaRespuestas = new List<string>();
-            //Metodo que arama la encuesta, y recibe como parametro la encuesta encontrada, y las listas de preguntas y respuestas
             armarEncuesta(encuestaEncontrada, listaPreguntas, listaRespuestas);
            
-            // enviar a la pantalla los datos del cliente y la llamada con el metodo pedirSeleccionGeneracionCsv()
             pantalla.pedirSeleccionGeneracionCsv(nombreCliente, dniCliente, celularCliente, estadoActual, duracionLlamada,listaPreguntas, listaRespuestas);
   
         }
-        //crear un metodo que se llame armarEncuesta(listaPreguntas,listaRespuesta)
         public void armarEncuesta(Encuesta encontrada,List<string> listaPreguntas , List<string> listaRespuesta)
         {
             foreach (var pregunta in encontrada._preguntas)
@@ -223,11 +187,7 @@ namespace PPAI_DSI
             generarCsv();
         }
 
-        /*Genera un csv con el siguiente formato:
-        - Encabezado: nombre del cliente, estado actual de la llamada, duración de la llamada.
-        - Preguntas: para cada pregunta la descripción de la pregunta, 
-        la descripción de la respuesta seleccionada por el cliente. 
-        Se incluye una marca para dar cierre a cada pregunta.*/
+        // Genera un csv
         public void generarCsv()
         {
             // Datos de ejemplo para escribir en el archivo CSV
@@ -253,7 +213,6 @@ namespace PPAI_DSI
             {
                 using (StreamWriter escritor = new StreamWriter(rutaArchivo))
                 {
-                    // Escribir los datos en el archivo CSV
                     foreach (string[] fila in datos)
                     {
                         escritor.WriteLine(string.Join(separador, fila));
